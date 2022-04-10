dc1eaab85f14bb8cf22ef6687affaccf76d7f053
JATIN HANS <you@example.com>
2022-04-10 02:12:35 -0400

    Final commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  clientID: '196c9587128a4e6ea6dc7caa4348522d',
+  clientSecret: '7924608eccc54f99a868b77298db01c0',
+  userAPIBase: 'https://web422assignment-server.herokuapp.com/api/user',
+};
